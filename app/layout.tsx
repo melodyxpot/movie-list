227cd68f854c516a5931b503a8dd3adaee396a78
melodyxpot <melodyxpot@gmail.com>
2024-10-23 19:11:36 +0200

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Next.js 14.2 with Tailwind CSS",
+	description:
+		"Next App Boilerplate with Tailwind CSS. Husky, prettier and eslint are installed."
+};
+
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={inter.className}>{children}</body>
+		</html>
+	);
+}
